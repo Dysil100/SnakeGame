520182120b9244dd420538877337895d509e90bf
Dysil100 <silatsamdylan@gmail.com>
2021-12-15 18:22:35 +0100

    new Version! new Method!

--- src/main/java/SnakeGame.java
+++
@@ -1,9 +0,0 @@
-import game.GameFrame;
-
-public  class SnakeGame {
-
-    public static void main(String[] args) throws InterruptedException {
-        new GameFrame();
-    }
-
-}
--- src/main/java/game/elements/Element.java
+++
@@ -1,8 +0,0 @@
-package game.elements;
-
-
-import java.awt.*;
-
-public interface Element {
-    void draw(Graphics graphics, int size);
-}
